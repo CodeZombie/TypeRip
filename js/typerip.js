5462b619310570f9405da3d7c05b9982be19e51e
Jeremy Clark <jeremyclark@live.ca>
2018-07-25 10:40:39 -0400

    Downloads as correct type
    Fonts now download using the correct type, instead of always as woff2.

--- js/typerip.js
+++ js/typerip.js
@@ -33,7 +33,9 @@ function ripFonts(url_, vue_, callback_) {
 		}catch(e){
             callback_({error: true, message: "Catastrophic Failure: Unexpected response. Check URL."}, vue_);
             return;		
-        }
+		}
+		
+		console.log(json);
         
         output.foundryname = json.family.foundry.name;
         output.familyname = json.family.name
@@ -46,7 +48,7 @@ function ripFonts(url_, vue_, callback_) {
 			var subfont_web_id = json.family.fonts[i].family.web_id;
 			var subfont_fvd = json.family.fonts[i].font.web.fvd;
 			var font_url = "https://use.typekit.net/pf/tk/" + subfont_web_id + "/" + subfont_fvd + "/m?primer=" + font_primer +  "&v=2&ec_token=3bb2a6e53c9684ffdc9a9bf71d5b2a620e68abb153386c46ebe547292f11a96176a59ec4f0c7aacfef2663c08018dc100eedf850c284fb72392ba910777487b32ba21c08cc8c33d00bda49e7e2cc90baff01835518dde43e2e8d5ebf7b76545fc2687ab10bc2b0911a141f3cf7f04f3cac438a135f";
-            output.fonts.push({url: font_url, name: json.family.fonts[i].name , foundry_name: json.family.foundry.name, foundry_slug: json.family.foundry.slug});
+            output.fonts.push({url: font_url, name: json.family.fonts[i].name , foundry_name: json.family.foundry.name, foundry_slug: json.family.foundry.slug, type: json.family.fonts[i].font.web.type});
         }		
         callback_({data: output, error: false}, vue_);
 	}
@@ -61,7 +63,7 @@ function ripFonts(url_, vue_, callback_) {
 	ajaxRequest.send();
 }
 
-function downloadFont(url_, fontName_) {
+function downloadFont(url_, fontName_, type_) {
 	/*
 	AJAX requests the font file from TypeKit.
 	creates an "a" dom element, setting the href attribute to an object representing the entire font as a blob.
@@ -73,7 +75,7 @@ function downloadFont(url_, fontName_) {
 	ajaxRequest.onload = function() {
 		var element = window.document.createElement('a');
 		element.href = window.URL.createObjectURL(ajaxRequest.response);
-		element.setAttribute('download', fontName_ + ".woff2");
+		element.setAttribute('download', fontName_ + "." + type_);
 		element.style.display = 'none';
 		document.body.appendChild(element)
 		element.click();
--- js/view.js
+++ js/view.js
@@ -3,7 +3,7 @@ var x = new Vue({
     data: {
         url_input: "",
         fonts: [],
-        message: {visible: true, text: "Welcome to Typerip: the Typekit font ripper. Enter a Typekit URL and click 'rip fonts'"}
+        message: {visible: true, text: "Welcome to Typerip: the <a target='_blank' href='http://typekit.com'>Typekit</a> font ripper.<br/><br/> <span style='font-size:.85em'>If a font appears corrupt after downloading, use a <a target='_blank' href='https://transfonter.org/'>font conversion tool</a> to fix it.</span> <br/>"}
     },
     methods: {
         showMessage: function(message_) {
@@ -24,13 +24,13 @@ var x = new Vue({
                         font_css.appendChild(document.createTextNode("@font-face { font-family: '" + font.name + "';\src: url(" + font.url + ");}"));
                         document.head.appendChild(font_css);
 
-                        v_.fonts.push({name: font.name, url: font.url, foundry_slug: font.foundry_slug, expanded: false});
+                        v_.fonts.push({name: font.name, url: font.url, foundry_slug: font.foundry_slug, expanded: false, type: font.type});
                     });
                 }
             });
         },
-        fontDownloadButtonOnClick: function(url_, name_) {
-            downloadFont(url_, name_);
+        fontDownloadButtonOnClick: function(url_, name_, type_) {
+            downloadFont(url_, name_, type_);
         }
     }
 });
