7156386ebc2ff472393f0b60a3b4816a702900d6
Jeremy Clark <cljeremy@sheridancollege.ca>
2020-03-09 17:10:19 -0400

    Opentype.js workarounds
    Opentype.js struggles to encode fonts that are anything but perfectly formatted. This commit adds two features which work around these limitations and fix several bugs in the process.

--- js/typerip.js
+++ js/typerip.js
@@ -2,31 +2,31 @@ var TypeRip = {
     getFontFamily: function(url_, callback_) {
         axios.get("https://cors-anywhere.herokuapp.com/" + url_)
         .then(function (response) {
-            var fontFamily = {
+            let fontFamily = {
                 name: "",
                 designers: [],
                 fonts: []
             }
 
             //search for the first part of the json
-            var json_start = response.data.search('{"family":{"slug":"'); 
+            let json_start = response.data.search('{"family":{"slug":"'); 
 		    if(json_start == -1) {
                 callback_("error", "Catastrophic Failure 001:  Unexpected response. Check URL.")
                 return
             }
 
             //cut off everything before this point
-            var data = response.data.substring(json_start)
+            let data = response.data.substring(json_start)
 
             //find the stuff directly after the json, and ues this as the anchor    
-            var json_end = data.search('</script>') 
+            let json_end = data.search('</script>') 
             if(json_end == -1) {
                 callback_("error", "Catastrophic Failure 002: Unexpected response. Check URL.")
                 return
             }
 
             //parse the json blob
-            var json;
+            let json;
             try {
                 json = JSON.parse(data.substring(0, json_end)); 
             }catch(e){
@@ -46,8 +46,8 @@ var TypeRip = {
             fontFamily.slug = json.family.slug
 
             //designers
-            for(var i = 0; i < json.family.designers.length; i++) {
-                var designer = {}
+            for(let i = 0; i < json.family.designers.length; i++) {
+                let designer = {}
                 designer["name"] = json.family.designers[i].name
 
                 if(json.designer_info[json.family.designers[i].slug] != null){
@@ -58,10 +58,10 @@ var TypeRip = {
             }
 
             //The primer is used for creating the font download URL
-            var primer = json.textSampleData.textSamplePrimers[Object.keys(json.textSampleData.textSamplePrimers)[0]];
+            let primer = json.textSampleData.textSamplePrimers[Object.keys(json.textSampleData.textSamplePrimers)[0]];
 
             //populate subfonts
-            for (var i = 0; i < json.family.fonts.length; i++) {
+            for (let i = 0; i < json.family.fonts.length; i++) {
                 fontFamily.fonts.push({
                     url: "https://use.typekit.net/pf/tk/" + json.family.fonts[i].family.web_id + "/" + json.family.fonts[i].font.web.fvd + "/m?primer=" + primer +  "&v=2&ec_token=3bb2a6e53c9684ffdc9a9bf71d5b2a620e68abb153386c46ebe547292f11a96176a59ec4f0c7aacfef2663c08018dc100eedf850c284fb72392ba910777487b32ba21c08cc8c33d00bda49e7e2cc90baff01835518dde43e2e8d5ebf7b76545fc2687ab10bc2b0911a141f3cf7f04f3cac438a135f", 
                     name: json.family.fonts[i].name,
@@ -83,52 +83,55 @@ var TypeRip = {
                 return "Error: Font failed to load."
             }else{
                 //Rebuild the glyph data structure. This repairs any encoding issues.
-                var rebuiltGlyphs = []
+                let rebuiltGlyphs = []
 
                 //for every glyph in the parsed font data:
-                for(var i = 0; i < fontData_.glyphs.length; i++) {
+                for(let i = 0; i < fontData_.glyphs.length; i++) {
                     //Create a structure to hold the new glyph data
-                    //populate it with some common fields:
-                    var glyphData = {
-                        name: fontData_.glyphs.glyphs[i].name,
-                        unicode: fontData_.glyphs.glyphs[i].unicode,
-                        path: fontData_.glyphs.glyphs[i].path
-                    }
+                    let glyphData = {};
 
-                    //Copy optional glyph fields, if they exist
-                    var optionalGlyphFields = ['advanceWidth', 'leftSideBearing']
-                    optionalGlyphFields.forEach(field => {
+                    let glyphFields = ['name', 'unicode', 'unicodes', 'path', 'index', 'advanceWidth', 'leftSideBearing']
+
+                    glyphFields.forEach(field => {
                         if(fontData_.glyphs.glyphs[i][field] != null) {
                             glyphData[field] = fontData_.glyphs.glyphs[i][field]
                         }
                     });
 
-                    //Rebuild the new glyph.
-                    var rebuiltGlyph = new opentype.Glyph(glyphData)
+                    //HOTFIX #1     If the advanceWidth of a glyph is NaN, opentype will crash.
+                    //SOLUTION:     Ensure advanceWidth has non-NaN AND non-0 value
+                    if(glyphData.advanceWidth == null || isNaN(glyphData.advanceWidth)){
+                        let newAdvanceWidth = Math.floor(fontData_.glyphs.glyphs[i].getBoundingBox().x2);
+                        if(newAdvanceWidth == 0){
+                            newAdvanceWidth = fontData_.glyphs.glyphs[0].getBoundingBox().x2;
+                        }
+                        glyphData.advanceWidth = newAdvanceWidth;
+                    }
 
-                    //Set all fields with the value of zero. Zero-value fields must be set AFTER the constructor
-                    //  due to a bug in OpenType.js https://github.com/opentypejs/opentype.js/issues/375
-                    optionalGlyphFields.forEach(field => {
-                        if(fontData_.glyphs.glyphs[i][field] != null) {
-                            if(fontData_.glyphs.glyphs[i][field] == 0){
-                                rebuiltGlyph[field] = 0
-                            }
+                    //Rebuild the new glyph.
+                    let rebuiltGlyph = new opentype.Glyph(glyphData);
+
+                    //HOTFIX #2:    If fields with a value of 0 are used in the constructor, opentype will simply not set them in the object.
+                    //SOLUTION:     Manually go through every 0 field that should have been set in the constructor, and set it. ( https://github.com/opentypejs/opentype.js/issues/375 )
+                    glyphFields.forEach(field => {
+                        if(glyphData[field] != null && glyphData[field] == 0) {
+                            rebuiltGlyph[field] = 0
                         }
                     })
-                    
+
                     //push the rebuilt glyph to an array.
                     rebuiltGlyphs.push(rebuiltGlyph)
                 }
                 
                 //create a structure of font data with fields from the parsed font.
-                var newFontData = {
+                let newFontData = {
                     familyName: font_.familyName,
                     styleName: font_.style,
                     glyphs: rebuiltGlyphs
                 }
 
                 //extract as much available data out of the existing font data and copy it over to the new font:
-                var optionalFontDataFields = ['defaultWidthX', 'nominalWidthX', 'unitsPerEm', 'ascender', 'descender' ]
+                let optionalFontDataFields = ['defaultWidthX', 'nominalWidthX', 'unitsPerEm', 'ascender', 'descender' ]
                 optionalFontDataFields.forEach(field => {
                     if(fontData_[field] != null) {
                         newFontData[field] = fontData_[field]
@@ -136,8 +139,8 @@ var TypeRip = {
                 });
 
                 //rebuild and download the font.
-                var newFont = new opentype.Font(newFontData)
-                
+                let newFont = new opentype.Font(newFontData)
+
                 try{
                     newFont.download()
                 }catch(e){
@@ -149,3 +152,4 @@ var TypeRip = {
         })
     }
 }
+
